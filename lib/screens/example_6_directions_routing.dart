b334c27acad4f77dacc5dfd9ca7e5474076fccdd
Karen Araqelyan <karen.araqelyan@hylink.am>
2025-04-02 18:51:54 +0400

    add directions and routing api call

--- lib/screens/example_6_directions_routing.dart
+++ lib/screens/example_6_directions_routing.dart
@@ -1,9 +1,12 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_sample_apps/constants.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:http/http.dart' as http;
 
 class Example6DirectionsRoutingScreen extends StatefulWidget {
   const Example6DirectionsRoutingScreen({super.key});
@@ -68,6 +71,76 @@ class _Example6DirectionsRoutingScreenState extends State<Example6DirectionsRout
         print("Error: ${result.errorMessage}");
       }
     }
+  }
+
+  Future<void> _getDirections() async {
+    final String url =
+        'https://maps.googleapis.com/maps/api/directions/json?origin=${_origin.latitude},${_origin.longitude}&destination=${_destination.latitude},${_destination.longitude}&key=$apiKey';
+
+    final response = await http.get(Uri.parse(url));
+    final data = json.decode(response.body);
+
+    if (data['routes'].isNotEmpty) {
+      final String polyline = data['routes'][0]['overview_polyline']['points'];
+      final List<LatLng> points = _decodePolyline(polyline);
+
+      setState(() {
+        _polylines.add(
+          Polyline(
+            polylineId: PolylineId('route'),
+            points: points,
+            color: Colors.blue,
+            width: 5,
+          ),
+        );
+      });
+    }
+  }
+
+  List<LatLng> _decodePolyline(String polyline) {
+    List<LatLng> points = [];
+    int index = 0;
+    int len = polyline.length;
+    int lat = 0;
+    int lng = 0;
+
+    // Ensure that the polyline string is not empty.
+    if (polyline.isEmpty) {
+      return points;
+    }
+
+    while (index < len) {
+      int shift = 0;
+      int result = 0;
+
+      // Decode latitude
+      do {
+        int b = polyline.codeUnitAt(index++) - 63;
+        result |= (b & 0x1f) << shift;
+        shift += 5;
+      } while (polyline.codeUnitAt(index - 1) >= 0x20);
+
+      int dlat = (result & 1) != 0 ? ~(result >> 1) : (result >> 1);
+      lat += dlat;
+
+      // Decode longitude
+      shift = 0;
+      result = 0;
+
+      do {
+        int b = polyline.codeUnitAt(index++) - 63;
+        result |= (b & 0x1f) << shift;
+        shift += 5;
+      } while (polyline.codeUnitAt(index - 1) >= 0x20);
+
+      int dlng = (result & 1) != 0 ? ~(result >> 1) : (result >> 1);
+      lng += dlng;
+
+      // Convert the lat and lng to real coordinates (scaled by 1E5)
+      points.add(LatLng(lat / 1E5, lng / 1E5));
+    }
+
+    return points;
   }
 
   @override
