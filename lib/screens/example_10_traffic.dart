d22e38ba3ea0c18629d1479351a98e3727e0014f
Karen Araqelyan <karen.araqelyan@hylink.am>
2025-04-03 11:43:09 +0400

    add gestures

--- lib/screens/example_10_traffic.dart
+++ lib/screens/example_10_traffic.dart
@@ -12,6 +12,10 @@ class _Example10TrafficScreenState extends State<Example10TrafficScreen> {
   late GoogleMapController _mapController;
   bool _showTraffic = false;
   bool _showBicycling = false;
+  bool _zoomGesturesEnabled = true;
+  bool _scrollGesturesEnabled = true;
+  bool _rotateGesturesEnabled = true;
+  bool _tiltGesturesEnabled = true;
 
   @override
   Widget build(BuildContext context) {
@@ -21,6 +25,10 @@ class _Example10TrafficScreenState extends State<Example10TrafficScreen> {
           GoogleMap(
             initialCameraPosition: CameraPosition(target: LatLng(37.7749, -122.4194), zoom: 12),
             trafficEnabled: _showTraffic,
+            zoomGesturesEnabled: _zoomGesturesEnabled,
+            scrollGesturesEnabled: _scrollGesturesEnabled,
+            rotateGesturesEnabled: _rotateGesturesEnabled,
+            tiltGesturesEnabled: _tiltGesturesEnabled,
             onMapCreated: (controller) => _mapController = controller,
           ),
           Align(
@@ -46,6 +54,34 @@ class _Example10TrafficScreenState extends State<Example10TrafficScreen> {
                             ? '[{"featureType": "all", "stylers": [{"visibility": "on"}]}]' // Show biking routes
                             : null); // Revert to default
                       });
+                    },
+                  ),
+                  SwitchListTile(
+                    title: Text('Zoom'),
+                    value: _zoomGesturesEnabled,
+                    onChanged: (value) {
+                      setState(() => _zoomGesturesEnabled = value);
+                    },
+                  ),
+                  SwitchListTile(
+                    title: Text('Scroll'),
+                    value: _scrollGesturesEnabled,
+                    onChanged: (value) {
+                      setState(() => _scrollGesturesEnabled = value);
+                    },
+                  ),
+                  SwitchListTile(
+                    title: Text('Rotate'),
+                    value: _rotateGesturesEnabled,
+                    onChanged: (value) {
+                      setState(() => _rotateGesturesEnabled = value);
+                    },
+                  ),
+                  SwitchListTile(
+                    title: Text('Tilt'),
+                    value: _tiltGesturesEnabled,
+                    onChanged: (value) {
+                      setState(() => _tiltGesturesEnabled = value);
                     },
                   ),
                 ],
