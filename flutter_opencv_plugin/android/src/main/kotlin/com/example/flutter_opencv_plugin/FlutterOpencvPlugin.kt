bdd21570d32767d1bef5ecc3e7d1f7203f7c5cb8
Karen Araqelyan <karen.araqelyan@hylink.am>
2025-07-11 18:25:22 +0400

    add android build without functionality

--- flutter_opencv_plugin/android/src/main/kotlin/com/example/flutter_opencv_plugin/FlutterOpencvPlugin.kt
+++ flutter_opencv_plugin/android/src/main/kotlin/com/example/flutter_opencv_plugin/FlutterOpencvPlugin.kt
@@ -1,33 +1,15 @@
 package com.example.flutter_opencv_plugin
 
+import androidx.annotation.NonNull
 import io.flutter.embedding.engine.plugins.FlutterPlugin
-import io.flutter.plugin.common.MethodCall
-import io.flutter.plugin.common.MethodChannel
-import io.flutter.plugin.common.MethodChannel.MethodCallHandler
-import io.flutter.plugin.common.MethodChannel.Result
 
 /** FlutterOpencvPlugin */
-class FlutterOpencvPlugin: FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
-
-  override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "flutter_opencv_plugin")
-    channel.setMethodCallHandler(this)
+class FlutterOpencvPlugin : FlutterPlugin {
+  override fun onAttachedToEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+    // No method channel setup needed since FFI is used
   }
 
-  override fun onMethodCall(call: MethodCall, result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
-    }
-  }
-
-  override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
+  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+    // Cleanup if needed
   }
 }
