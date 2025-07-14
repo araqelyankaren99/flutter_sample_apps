c48ad969e0c4067f2fae3c24c87d412568e51c88
Karen Araqelyan <karen.araqelyan@hylink.am>
2025-07-14 16:05:08 +0400

    add initial version

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,6 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      builder: (_, child) => TopNotificationManagerWidget(child: child),
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -113,10 +117,152 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: showOverlay,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  void showOverlay() {
+    final text = Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Center(child: Text('The lecture has been added'),),
+    );
+    context.read<TopNotificationManager>().show('Hello my friend');
+  }
+}
+
+class TopNotificationOverlayWidget extends StatefulWidget {
+  const TopNotificationOverlayWidget({super.key});
+
+  @override
+  State<TopNotificationOverlayWidget> createState() =>
+      TopNotificationOverlayWidgetState();
+}
+
+class TopNotificationOverlayWidgetState
+    extends State<TopNotificationOverlayWidget> {
+  static const _showedOffset = Offset(0, 0);
+  static const _hidedOffset = Offset(0, -1);
+
+  Timer? _timer;
+  var _text = "";
+  var _offset = _hidedOffset;
+  final List<String> _textsQueue = <String>[];
+
+  bool get isHidien => _offset == _hidedOffset;
+
+  void show(String text) {
+    if (!isHidien) {
+      _timer?.cancel();
+      _timer = null;
+      _textsQueue.add(text);
+      _hideWidget();
+    } else {
+      _showWidget(text);
+    }
+  }
+
+  void _showWidget(String text) {
+    setState(() {
+      _text = text;
+      _offset = _showedOffset;
+    });
+    _timer = Timer(const Duration(seconds: 5), () {
+      _timer = null;
+      _hideWidget();
+    });
+  }
+
+  void _hideWidget() {
+    setState(() {
+      _offset = _hidedOffset;
+    });
+  }
+
+  void _onAnimationEnd() {
+    if (_textsQueue.isEmpty || !isHidien) {
+      return;
+    }
+    final text = _textsQueue.first;
+    _textsQueue.removeAt(0);
+    _showWidget(text);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final topPadding = MediaQuery.of(context).padding.top;
+    return Positioned(
+      top: 0,
+      left: 0,
+      right: 0,
+      child: AnimatedSlide(
+        duration: const Duration(milliseconds: 150),
+        offset: _offset,
+        onEnd: _onAnimationEnd,
+        child: Material(
+          child: Container(
+            padding: EdgeInsets.only(top: topPadding),
+            color: Colors.green,
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Center(
+                child: Text(
+                  _text,
+                  style :TextStyle(color: Colors.black),
+                  maxLines: null,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+abstract class TopNotificationManager {
+  void show(String text);
+}
+
+class TopNotificationManagerWidget extends StatefulWidget {
+  final Widget? child;
+  const TopNotificationManagerWidget({
+    super.key,
+    this.child,
+  });
+
+  @override
+  State<TopNotificationManagerWidget> createState() => _TopNotificationManagerWidgetState();
+}
+
+class _TopNotificationManagerWidgetState
+    extends State<TopNotificationManagerWidget>
+    implements TopNotificationManager {
+  final _topNotificationOverlayKey =
+  GlobalKey<TopNotificationOverlayWidgetState>();
+  @override
+  Widget build(BuildContext context) {
+    var children = <Widget>[];
+    final child = widget.child;
+    if (child != null) {
+      children.add(child);
+    }
+    final topNotificationOverlayWidget = TopNotificationOverlayWidget(
+      key: _topNotificationOverlayKey,
+    );
+    children.add(topNotificationOverlayWidget);
+    return Provider<TopNotificationManager>.value(
+      value: this,
+      child: Stack(
+        children: children,
+      ),
+    );
+  }
+
+  @override
+  void show(String text) {
+    _topNotificationOverlayKey.currentState?.show(text);
   }
 }
